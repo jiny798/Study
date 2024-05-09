b2c3c0f4573b365484260c39397f42b5f9c36c74
jyoung7988 <jyoung7988@naver.com>
2024-05-09 23:41:42 +0900

    v-model , v-on:click 사용

--- vue/vue-todo/src/components/TodoInput.vue
+++ vue/vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    
+    <input type="text" v-model="newTodoItem"> <!--v-model = data 값과 동기화 가능  -->
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function(){
+    return{
+      newTodoItem: ""
+    }
+  },
+  methods:{
+    addTodo:function(){
+      console.log(this.newTodoItem); // this -> TodoInput 컴포넌트
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
