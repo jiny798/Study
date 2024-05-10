884a8a3e2e915691514e47f05e1b26df282fd79d
jyoung7988 <jyoung7988@naver.com>
2024-05-10 23:55:14 +0900

    enter 이벤트 사용

--- vue/vue-todo/src/components/TodoInput.vue
+++ vue/vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem"> <!--v-model = data 값과 동기화 가능  -->
-    <button v-on:click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo"> <!--v-model = data 값과 동기화 가능  -->
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="">+</i>
+    </span>
   </div>
 </template>
 
@@ -16,12 +19,43 @@ export default {
     addTodo:function(){
       console.log(this.newTodoItem); // this -> TodoInput 컴포넌트
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function(){
       this.newTodoItem = '';
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox{
+  background: white;
+  height: 50px; 
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer{
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 
 </style>
