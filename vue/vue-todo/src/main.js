683316932cad3d174ea231e818027f993a3bb29b
jyoung7988 <jyoung7988@naver.com>
2024-05-16 00:16:04 +0900

    router 설치

--- vue/vue-todo/src/main.js
+++ vue/vue-todo/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { router } from './router/index.js';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
@@ -11,5 +13,7 @@ Vue.config.productionTip = false;
 
 new Vue({
     store,
+    // router: router, -> router 하나로 축약 가능 ES 문법
+    router,
     render: (h) => h(App),
 }).$mount('#app');
