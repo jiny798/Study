0f2518328afb07bddc7ccdfafb1ec95f7dbbaa01
jyoung7988 <jyoung7988@naver.com>
2024-05-18 19:38:33 +0900

    style

--- vue/vue-todo/src/views/NewsView.vue
+++ vue/vue-todo/src/views/NewsView.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div v-for="user in this.$store.state.news" :key="user.id">{{ user.title }}</div>
+        <p v-for="item in this.$store.state.news">
+            <a :href="item.url">{{ item.title }}</a>
+            <small>{{ item.time_ago }} by {{ item.user }}</small>
+        </p>
     </div>
 </template>
 
@@ -8,22 +11,8 @@
 // import { fetchNewsList } from '../api/index.js';
 
 export default {
-    // data() {
-    //     return {
-    //         users: [],
-    //     };
-    // },
     created() {
         this.$store.dispatch('FETCH_NEWS'); // action 수행
-        // var vm = this;
-        // fetchNewsList()
-        //     .then(function (response) {
-        //         console.log(response);
-        //         vm.users = response.data;
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
     },
 };
 </script>
