ffbf09f177180fa712f42587fb0d7a50f8ce4009
jyoung7988 <jyoung7988@naver.com>
2024-07-29 23:01:00 +0900

    요청 기반 권한 부여 방식

--- spring/src/main/java/hello/itemservice/subject/security/step8/README.md
+++ spring/src/main/java/hello/itemservice/subject/security/step8/README.md
@@ -1,2 +1,5 @@
 ### 인가 프로세스 
 
+### securityMatcher()
+
+### securityMatchers(Customizer<RequestMatcherConfigurer>)
--- spring/src/main/java/hello/itemservice/subject/security/step8/SecurityConfig.java
+++ spring/src/main/java/hello/itemservice/subject/security/step8/SecurityConfig.java
@@ -3,6 +3,7 @@ package hello.itemservice.subject.security.step8;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -55,13 +56,38 @@ public class SecurityConfig {
 	// 	return http.build();
 	// }
 
+	// @Bean
+	// SecurityFilterChain defaultFilterChain(HttpSecurity http, ApplicationContext context) throws Exception {
+	//
+	// 	http
+	// 		.authorizeHttpRequests(auth -> auth
+	// 			.requestMatchers(new CustomRequestMatcher("/user")).hasAuthority("ROLE_USER")
+	// 			.anyRequest().authenticated())
+	// 		.formLogin(Customizer.withDefaults());
+	//
+	// 	return http.build();
+	// }
+
+
+	//SecurityMatcher
 	@Bean
-	SecurityFilterChain defaultFilterChain(HttpSecurity http, ApplicationContext context) throws Exception {
+	SecurityFilterChain filterChain1(HttpSecurity http, ApplicationContext context) throws Exception {
 
 		http
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(new CustomRequestMatcher("/user")).hasAuthority("ROLE_USER")
 				.anyRequest().authenticated())
+			.formLogin(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(1) // 위 필터체인에 @Order(1) 를 넣으면 다른 결과 발생, 생성 순서에 따라 처리
+	SecurityFilterChain filterChain2(HttpSecurity http, ApplicationContext context) throws Exception {
+
+		http.securityMatchers(matchers -> matchers.requestMatchers("/api/**", "/api2/**"))
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll())
 			.formLogin(Customizer.withDefaults());
 
 		return http.build();
