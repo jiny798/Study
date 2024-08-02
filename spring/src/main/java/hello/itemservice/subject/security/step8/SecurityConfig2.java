c4d37ce80e803bd88fd234ce762e5e3ee87e53a9
jyoung7988 <jyoung7988@naver.com>
2024-08-02 23:45:56 +0900

    계층적 권한

--- spring/src/main/java/hello/itemservice/subject/security/step8/SecurityConfig2.java
+++ spring/src/main/java/hello/itemservice/subject/security/step8/SecurityConfig2.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,6 +46,13 @@ public class SecurityConfig2 {
 		return http.build();
 	}
 
+	@Bean
+	public RoleHierarchy roleHierarchy() {
+		RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+		roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER\n" +
+			"ROLE_USER > ROLE_ANONYMOUS");
+		return roleHierarchy;
+	}
 	@Bean
 	public UserDetailsService userDetailsService(){
 		UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
