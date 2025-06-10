d5ba6586f00cebb5459cfcaa43cd54af85aa50cd
jiny <jyoung7988@naver.com>
2025-06-10 23:28:36 +0900

    자바 코드로 요청 필터 적용

---
+++ micro-service/apigateway-service/src/main/java/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-value")
+                        .addResponseHeader("first-response", "first-value"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-value")
+                                .addResponseHeader("second-response", "second-value"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
